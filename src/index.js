a5b475e52e58beb335d08773be74247a66b5ca21
shuaixiaocheng <shuaixiaocheng@antiy.cn>
2018-09-26 11:37:59 +0800

    redux

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import store from './store';
+import { addToCart } from './actions/cart-actions';
+import { updateCart } from './actions/cart-actions';
+import { deleteFromCart } from './actions/cart-actions';
+
+
+console.log("initial state: ", store.getState());
+
+
+let unsubscribe = store.subscribe(() =>
+    console.log(store.getState())
+);
+
+
+
+store.dispatch(addToCart('Coffee 500gm', 1, 250));
+store.dispatch(addToCart('Flour 1kg', 2, 110));
+store.dispatch(addToCart('Juice 2L', 1, 250));
+
+
+
+// Update Cart
+store.dispatch(updateCart('Flour 1kg', 5, 110));
+
+// Delete from Cart
+store.dispatch(deleteFromCart('Coffee 500gm'));
+
+unsubscribe();
---
+++ src/store.js
@@ -0,0 +1,24 @@
+
+import { createStore } from 'redux';
+
+import rootReducer from './reducers/index';
+
+
+/* eslint-disable no-underscore-dangle */
+let store = createStore(
+    rootReducer
+    , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
+
+export default store;
+
+
+
+
+
+
+
+
+
+
